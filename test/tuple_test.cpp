0cad6dcd65c9f28c711a34e923234b794af25345
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-04-24 11:13:24 +0530

    Subtraction and negation tests

--- test/tuple_test.cpp
+++ test/tuple_test.cpp
@@ -1,6 +1,4 @@
 #include "util.hpp"
-#include "vector.hpp"
-#include "point.hpp"
 
 #include <gtest/gtest.h>
 
@@ -44,3 +42,33 @@ TEST(TupleTest, PointEqTest) {
     auto v = rtc::vector<float>(4, -4, 3);
     ASSERT_EQ(v, rtc::tuple<float>(4, -4, 3, 0));
 }
+
+TEST(TupleTest, SubtractPoints) {
+    const rtc::point<float> p1(3, 2, 1);
+    const rtc::point<float> p2(5, 6, 7);
+    ASSERT_EQ(p1 - p2, rtc::vector<float>(-2, -4, -6));
+}
+
+TEST(TupleTest, SubtractVectorPoint) {
+    const rtc::point<float> p(3, 2, 1);
+    const rtc::vector<float> v(5, 6, 7);
+    ASSERT_EQ(p - v, rtc::point<float>(-2, -4, -6));
+}
+
+TEST(TupleTest, SubtractVectors) {
+    const rtc::vector<float> v1(3, 2, 1);
+    const rtc::vector<float> v2(5, 6, 7);
+    ASSERT_EQ(v1 - v2, rtc::vector<float>(-2, -4, -6));
+}
+
+TEST(TupleTest, Negate) {
+    const rtc::tuple<float> a(1, -2, 3, -4);
+    ASSERT_EQ(-a, rtc::tuple<float>(-1, 2, -3, 4));
+}
+
+TEST(TupleTest, Scaling) {
+    const rtc::tuple<float> a(1, -2, 3, -4);
+    ASSERT_EQ(a*3.5, rtc::tuple<float>(3.5, -7, 10.5, -14));
+    ASSERT_EQ(a*0.5, rtc::tuple<float>(0.5, -1, 1.5, -2));
+}
+
